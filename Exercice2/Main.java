1173a1d66c841a1be55ef1610cfdb3a24be569d4
lepepito <agarprogo90@gmail.com>
2026-03-15 16:29:18 +0000

    Clean up comments in Main.java
    Removed comments for initialization and translation steps.

--- Exercice2/Main.java
+++ Exercice2/Main.java
@@ -2,18 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Point p1 = new Point();
         
-        // Initialisation et premier affichage
         p1.initialiser('A', 2.5);
         p1.affiche();
         
-        // Déplacement (translation) et deuxième affichage
         p1.translate(3.0);
         p1.affiche();
         
-        // Bonus : test de la deuxième méthode initialiser
-        Point p2 = new Point();
-        p2.initialiser(p1);
-        System.out.print("Copie -> ");
-        p2.affiche();
     }
 }
