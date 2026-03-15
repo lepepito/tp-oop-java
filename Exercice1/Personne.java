2537e49ace66b54dae02a49f3b42613cb63246f4
lepepito <agarprogo90@gmail.com>
2026-03-15 16:23:56 +0000

    Refactor Personne class methods and fields

--- Exercice1/Personne.java
+++ Exercice1/Personne.java
@@ -1,26 +1,28 @@
 public class Personne {
-    String CIN;
+    // 1. Les champs
+    String cin;
     String nom;
     String prenom;
     String email;
     int age;
-    public void initialiser(String CIN,String nom,String prenom,String email,int age){
-    this.CIN = CIN;
-    this.nom = nom;
-    this.prenom = prenom;
-    this.email = email;
-    this.age = age;
+
+    // 2. La fonction pour initialiser les valeurs
+    public void initialiser(String c, String n, String p, String e, int a) {
+        cin = c;
+        nom = n;
+        prenom = p;
+        email = e;
+        age = a;
     }
-    public void afficher(){
-        System.out.println(this.CIN);
-        System.out.println(this.nom);
-        System.out.println(this.prenom);
-        System.out.println(this.email);
-        System.out.println(this.age);
+
+    // 3. La fonction pour afficher les informations
+    public void afficherPersonne() {
+        System.out.println("--- Profil de la personne ---");
+        System.out.println("CIN : " + cin);
+        System.out.println("Nom : " + nom);
+        System.out.println("Prénom : " + prenom);
+        System.out.println("Email : " + email);
+        System.out.println("Âge : " + age + " ans");
+        System.out.println("-----------------------------");
     }
-}
-public void main(String[] args){
-    Personne per1 = new Personne();
-    per1.initialiser("HH90505", "Saad", "Hossainy", "hosss@gmail.com", 20);
-    per1.afficher();
 }
