140285971b0dcda3a2719371c197207c3c52ec33
lepepito <agarprogo90@gmail.com>
2026-03-15 16:36:01 +0000

    Create main.java

---
+++ Exercice2/main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        
+
+        p1.initialiser('A', 2.5);
+        p1.affiche();
+        
+
+        p1.translate(3.0);
+        p1.affiche();
+        
+
+    }
+}
